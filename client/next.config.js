820a40fc7528c02db89784e8729b91e652b0fb7b
Manish Kumar2 <manishkumar2@policybazaar.com>
2022-01-21 22:54:39 +0530

    Authentication completed

---
+++ client/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: "NODE-REACT-AWS",
+    API: "http://localhost:8000/api",
+    PRODUCTION: false,
+    DOMAIN: "http://localhost:3000",
+    FB_APP_ID: "JJSLKADFLKSAHFDSLKL",
+  },
+};
