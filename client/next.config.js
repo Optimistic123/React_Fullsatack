e1a5c393b67e84eaa10460b2795f991fa7b92255
Manish Kumar2 <manishkumar2@policybazaar.com>
2022-01-26 18:48:02 +0530

    aws file is removed

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicRuntimeConfig: {
     APP_NAME: "NODE-REACT-AWS",
-    API: "http://localhost:8000/api",
+    API: "https://fathomless-dawn-37877.herokuapp.com/api",
     PRODUCTION: false,
     DOMAIN: "http://localhost:3000",
     FB_APP_ID: "JJSLKADFLKSAHFDSLKL",
